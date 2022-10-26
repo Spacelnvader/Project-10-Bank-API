02ec2c83fd64063571491e10fc39a4f39ec6cfbe
Spacelnvader <la-douce-64@hotmail.fr>
2022-10-26 13:20:57 +0200

    rectification requete async avec try+catch

--- FrontEnd/src/features/auth/authService.js
+++ FrontEnd/src/features/auth/authService.js
@@ -10,36 +10,57 @@ const API_URL = 'http://localhost:3001/api/v1/user/'
 // Login user : get datas from backend
 const login = async (formData) => {
     try {
-    const res = await axios.post(API_URL + 'login', formData)
-        
-            //console.log(res.data.body.token)
-            if (res.data) { localStorage.setItem("token", res.data.body.token) }
-            return res.data
-        
+        const res = await axios.post(API_URL + 'login', formData)
+
+        //console.log(res.data.body.token)
+        if (res.data) { localStorage.setItem("token", res.data.body.token) }
+        return res.data
     }
-
-    catch(error){
-
+    catch (error) {
         console.log(error)
         throw new Error(error)
     }
-        
+
 }
 
+
+//profile user : get data user's profile with the token from backend
+// const userProfile = async (profileData, token) => {
+//     const config = {
+//         headers: {
+//             Authorization: `Bearer ${token}`
+//         }
+//     }
+//     return await axios.post(API_URL + 'profile', profileData, config)
+//         .then((res) => {
+//             return res.data.body
+//         })
+//         .catch((error) => console.log(error))
+// }
+
+// return await axios.put(API_URL + 'profile', newData, config)
+// .then((res) => {
+//     console.log(res.data.body)
+//     return res.data.body
+// })
+// .catch((error) => console.log(error))
 
 //profile user : get data user's profile with the token from backend
 const userProfile = async (profileData, token) => {
     const config = {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
+            Authorization: `Bearer ${token}`,
+        },
+    };
+    try {
+        const response = await axios.post(API_URL + 'profile', profileData, config);
+
+        return response.data.body;
+    } catch (e) {
+        console.log(e);
+        throw new Error(e);
     }
-    return await axios.post(API_URL + 'profile', profileData, config)
-        .then((res) => {
-            return res.data.body
-        })
-        .catch((error) => console.log(error))
-}
+};
 
 //update user profile 
 const updateUserData = async (newData, token) => {
@@ -47,13 +68,15 @@ const updateUserData = async (newData, token) => {
         headers: {
             Authorization: `Bearer ${token}`
         }
+    };
+    try  {
+        const response = await axios.put(API_URL + 'profile', newData, config);
+
+        return  response.data.body;
+    } catch (e) {
+        console.log(e)
+        throw new Error(e);
     }
-    return await axios.put(API_URL + 'profile', newData, config)
-        .then((res) => {
-            console.log(res.data.body)
-            return res.data.body
-        })
-        .catch((error) => console.log(error))
 }
 
 // Logout user
